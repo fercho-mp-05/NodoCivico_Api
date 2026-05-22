8b36e9bd0406ace97d981af351aef0b36d417b88
fercho-mp-05 <ing.fernando.mp@gmail.com>
2026-05-22 16:09:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-aa
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+reports = []
+
+@app.route("/")
+def home():
+    return "API Nodo Civico funcionando"
+
+@app.route("/reports", methods=["GET"])
+def get_reports():
+    return jsonify(reports)
+
+@app.route("/reports", methods=["POST"])
+def create_report():
+    data = request.json
+    reports.append(data)
+    return jsonify({
+        "message": "Reporte creado",
+        "data": data
+    }), 201
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
